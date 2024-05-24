724e87fce5d97a332144d0e7a64ace04598f81ce
timothy ige <timoige87@gmail.com>
2024-05-24 01:10:16 +0000

    update Jenkinsfile

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "aws_vpc" "my_vpc-2" {
 
 resource "aws_subnet" "subnet_1" {
   vpc_id     = aws_vpc.my_vpc-1.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.0.0/24"
   availability_zone = "us-east-1a"
   tags = {
     Name = "subnet_1"
